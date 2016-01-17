cdb5685a6cc6e99c0eefd20ac40bfa7ec8ce091d
Jesse Furmanek <jesse.furmanek@gmail.com>
2016-01-16 19:03:06 -0600

    Updates shell script and adds a lil readme

--- makesymlinksfordotfiles.sh
+++ makesymlinksfordotfiles.sh
@@ -24,8 +24,8 @@ echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/$olddir/
+    echo "Moving any existing dotfiles from ~ to $olddir/"
+    mv ~/$file /$olddir/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $dir/$file ~/$file
 done

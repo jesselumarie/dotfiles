16e1e2f07064a2c8dceb4c8d6880c57155ec81c1
Jesse Lumarie <jesse.lumarie@gmail.com>
2018-09-13 15:30:22 +0100

    Add .config to sync

--- sync.sh
+++ sync.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_backup           # old dotfiles backup directory
-files=".bash_profile .bashrc .vimrc .rspec .gitconfig .atom .gitignore_global .config/karabiner/karabiner.json"    # list of files/folders to symlink in homedir
+files=".bash_profile .bashrc .vimrc .rspec .gitconfig .atom .gitignore_global .config"    # list of files/folders to symlink in homedir
 
 ##########
 
